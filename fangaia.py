c9187b232d9ba6aef40e13cd47608d4c0111354b
MWieland <mwieland@gfz-potsdam.de>
2016-02-15 16:59:14 +0100

    worked on cooper jacob

--- fangaia.py
+++ fangaia.py
@@ -3,13 +3,13 @@
         fangaia.py
 ---------------------------
 Created on 27.01.2016
-Last modified on 29.01.2016
+Last modified on 15.02.2016
 Author: Marc Wieland
 Description: Hydraulic refill test to derive conductivity from waterlevel measurements.
              0. Resample and/or slice waterlevel timeseries (optional).
              1. Auto-detect drawdown events in waterlevel timeseries.
              2. Compute Hydraulic conductivity (K) as follows: 
-                 K= r / 4 dt * 2.303 * log(h1h2) 
+                 K= r / 4 * dt * 2.303 * log(h1h2) 
                  r = radius (constant from field  experiment)
                  h1: waterlevel at start of an event (after refill)
                  h2: waterlevel at end of an event (after drawdown)
@@ -36,10 +36,11 @@ sr = "D" # temporal resampling rate (e.g., 5min, H, D, M)
 stat = 'mean' # temporal resampling statistics 
 ###
 t_slice = False    # temporal slicing
-slice_t1 = '10/01/2013 00:00'   # slicing start time
-slice_t2 = '11/30/2013 00:00'   # slicing end time
+
+slice_t1 = '2013-01-10 00:00'   # slicing start time
+slice_t2 = '2013-12-31 00:00'   # slicing end time
 ###
-peak_lookahead = 200 #200     # distance to look ahead from a peak candidate to determine if it is the actual peak
+peak_lookahead = 100 #200     # distance to look ahead from a peak candidate to determine if it is the actual peak
 ###################################################################################################################
 
 starttime=time.time()
@@ -72,28 +73,28 @@ if t_slice is True:
 events = helper.drawdown_event_detection(wL, peak_lookahead)
 
 # Convert dt from [ns] to [s]
-dt = np.array((events['dt'].values / 1000000000), dtype=np.float64)
+dts = np.array((events['dt'].values / 1000000000), dtype=np.float64)
 
 ##############################
 ### Hydraulic conductivity ###
 ##############################
 # Compute hydraulic conductivity (K) and convert from [m/s] to [Darcy]
-events['K'] = r / (4 * np.pi * dt) * 2.303 * np.log(events['h1'] / events['h2'])
+events['K'] = r / (4 * dts) * 2.303 * np.log(events['h1'] / events['h2'])
 events['K'] = events['K'] * 100000
 
 ########################################
 ### Corrected hydraulic conductivity ###
 ########################################
-# Lookup monthly evapotranspiration values (ETpot) for the event start (h1_t) and end months (h2_t)
-events['et'] = events['h2_t'].dt.month.replace(range(1,13), df_ev['ETpot'].tolist())
+# Lookup monthly evapotranspiration values (ETpot) for the event end months (h2_t)
+events['h2_et'] = events['h2_t'].dt.month.replace(range(1,13), df_ev['ETpot'].tolist())
+events['h2_et'] = events['h2_et'] / 10  # Convert evapotranspiration values (h2_et) from [mm] to [cm]
 
 # Correct h2 waterlevel with ET values for each event 
-# TODO: check formula with Heiko (this is one to one what he did in excel)
-dtd = dt / 86400 # convert dt from [s] to [days] and c
-events['h2corr'] = events['h2'] + events['et'] / 1000 * dtd / 30.25
+dtd = dts / 86400 # Convert dt from [s] to [days]
+events['h2corr'] = events['h2'] + events['h2_et'] * dtd / 30.25
 
 # Compute hydraulic conductivity (K) and convert from [m/s] to [Darcy]
-events['Kcorr'] = r / (4 * np.pi * dt) * 2.303 * np.log(events['h1'] / events['h2corr'])
+events['Kcorr'] = r / (4 * dts) * 2.303 * np.log(events['h1'] / events['h2corr'])
 events['Kcorr'] = events['Kcorr'] * 100000
 print events
 
@@ -120,11 +121,11 @@ fig = plt.figure()
 if t_res is True:
     plt.plot(wL.index, wL.values, 'b-', label='water level')
     plt.title('Solfatara Fangaia (ceraDIVER - ' + sr + ' ' + stat + ')')
-    plt_save = wdir + 'fang_wL_T' + sr + stat + '.png'
+    plt_save = wdir + 'fang_wL_events_' + sr + stat + '.png'
 else:
     plt.plot(wL.index, wL.values, 'b-', label='water level')
     plt.title('Solfatara Fangaia (ceraDIVER)')
-    plt_save = wdir + 'fang_wL_T.png'  
+    plt_save = wdir + 'fang_wL_events.png'  
 # Add event markers
 plt.plot(events['h1_t'], events['h1'], 'r+', mew=2, ms=8, label='h1')
 plt.plot(events['h2_t'], events['h2'], 'g+', mew=2, ms=8, label='h2')
@@ -143,15 +144,16 @@ if t_res is True:
     plt.plot(events['h1_t'], events['K'], 'ro', mew=2, ms=8, label='hyd. cond.')
     plt.plot(events['h1_t'], events['Kcorr'], 'go', mew=2, ms=8, label='hyd. cond. corr.')
     plt.title('Solfatara Fangaia (ceraDIVER - ' + sr + ' ' + stat + ')')
-    plt_save = wdir + 'fang_K_T' + sr + stat + '.png'
+    plt_save = wdir + 'fang_K_' + sr + stat + '.png'
 else:
     plt.plot(events['h1_t'], events['K'], 'ro', mew=2, ms=8, label='hyd. conductivity')
     plt.plot(events['h1_t'], events['Kcorr'], 'go', mew=2, ms=8, label='hyd. cond. corr.')
     plt.title('Solfatara Fangaia (ceraDIVER)')
-    plt_save = wdir + 'fang_K_T.png'  
+    plt_save = wdir + 'fang_K.png'  
 plt.xticks(rotation=15)
 plt.xlabel('Time')
 plt.ylabel('K [Darcy]')
+plt.ylim(0, 0.25)
 plt.legend(loc='best')
 plt.grid()
 plt.tight_layout()
--- pisciarelli.py
+++ pisciarelli.py
@@ -2,22 +2,25 @@
 ---------------------------
     pisciarelli.py
 ---------------------------
-Created on 26.01.2016
-Last modified on 26.01.2016
+Created on 08.02.2016
+Last modified on 09.02.2016
 Author: Marc Wieland
-Description:      
-Reference: Transmissivity (T) is estimated from the pumping rate (Q) and the change in drawdown 
-             per log-cycle (Delta s) from the following equation:   T = 2.3 * Q / 4 * Pi * Delta s
-             where, Delta s is the change in drawdown per log-cycle (L).
-             
-             
-             r = distance from the discharging well
-             t = time elapsed since start of discharge
-             T = transmissibility of the aquifier
-             S = coefficient of storage
-             Q = discharge of the well
-             
-TODO: so far only a few test plots and an example formula. to be finished        
+Description: Single-well pump test to derive transmissivity from waterlevel measurements.
+             0. Resample and/or slice waterlevel and airpressure timeseries (optional).
+             1. Correct waterlevel with airpressure data
+             2. Auto-detect drawdown events in waterlevel timeseries.
+             3. Compute Transmissivity (T) as follows: 
+                 T = 2.3 * Q / 4 * Pi * dsw
+                 Q = pumping rate
+                 dsw: change in drawdown per log-cycle      
+Reference: Cooper and Jacob (1946): A generalized method for evaluating formation constants
+           and summarizing well-field history. Transactions of the American Geophysical
+           Union, 27 (5), 526-534.
+
+TODO: x 1. test plot raw wL data (and also corrected wL data) for April 10,2014 -> see if there are bumps
+      2. interpolate timeseries data (e.g., spline) to 5min (1min)
+      3. extract all wL values into csv for A) pump events (between min->max) and 
+                                            B) drawdown events (between max->min)
 ----
 '''
 
@@ -25,71 +28,190 @@ import time
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import timeseries.helper as helper
 
 # Parameters to set ########################################################################################################
 dat_wl = 'PITC (DCX22).csv'
 dat_bar = 'AGN3baro.csv'
 wdir = '/media/datadrive_/projects/medsuv_heiko/exp_pitc/marc/'
-L = "D" # temporal resampling rate (hour, day, month, etc.)
+###
+t_res = True # temporal resampling 
+sr = "15min" # temporal resampling rate (e.g., 5min, H, D, M)
 stat = 'mean' # temporal resampling statistics 
+###
+t_slice = True    # temporal slicing
+slice_t1 = '2014-09-20 00:01'   # slicing start time
+slice_t2 = '2014-09-20 23:59'   # slicing end time
+###
+peak_lookahead = 1 #200     # distance to look ahead from a peak candidate to determine if it is the actual peak
+###
+Q = 18  # Pumping rate [cubic meters per hour]
 ############################################################################################################################
 
 starttime=time.time()
 print 'Starttime : ' + str(time.strftime("%H:%M:%S"))
 
-# read data from csv to pandas dataframe
+####################################
+### Data input and preprocessing ###
+####################################
+# Read data from csv to pandas dataframe
 df1 = pd.read_table(wdir + dat_wl, header=0, delimiter=';', parse_dates=True) 
 df2 = pd.read_table(wdir + dat_bar, header=0, delimiter=';', parse_dates=True) 
 
-# create waterlevel timeseries
+# Create waterlevel timeseries
 dates1 = pd.DatetimeIndex(df1['longDATE'].values)
+df1['wL#'] = df1['wL#'] / 100   # convert waterlevel (wL) from [cm] to [m]
 wL = pd.Series(df1['wL#'].values, index=dates1)
 
-# create airpressure timeseries
+# Create airpressure timeseries
 dates2 = pd.DatetimeIndex(df2['longDATE'].values)
+#df2['aP'] = df2['aP'] * 10.19716    # convert airpressure (aP) from [bar] to [mH2O]
+df2['aP'] = df2['aP'] / 100     # convert airpressure (aP) from [cmH2O] to [mH2O]
 aP = pd.Series(df2['aP'].values, index=dates2)
 
-# slice timeseries
-#wL = wL['08/01/2014 00:00':'08/31/2014 23:59']
-#aP = aP['08/01/2014 00:00':'08/31/2014 23:59']
+if t_res is True:
+    # Resample timeseries
+    wL = wL.resample(sr, how=stat)
+    aP = aP.resample(sr, how=stat)
+    
+if t_slice is True:
+    # Slice timeseries
+    wL = wL[slice_t1:slice_t2]
+    aP = aP[slice_t1:slice_t2]
 
-#######################################################
-# compute transmissivity (T) per time interval (Cooper and Jacob 1946)
-# TODO: where do i get Q from? how is ds computed? how do i pick the events?
-ds = np.abs(wL.resample(L, how='min') - wL.resample(L, how='max'))
-T = 2.303 * Q / 4 * np.pi * ds
-#print T
+# Correct waterlevel with airpressure data
+wL = wL - aP
 
-# compute hydraulic conductivity (K) per time interval (Cooper and Jacob 1946)
-#dt = 30
-#r = 5
-#K = r / (4 * np.pi * dt) * 2.303 * np.log(wL.resample(L, how='max') / wL.resample(L, how='min'))
-#print K
+#######################
+### Event detection ###
+#######################
+# Detect drawdown events in waterlevel timeseries and return h1, h2 and dt
+events = helper.drawdown_event_detection(wL, peak_lookahead)
+print events
 
-#######################################################
-# plot waterlevel timeseries
+##########################################################
+### Compute transmissivity (T) (Cooper and Jacob 1946) ###
+##########################################################
+# TODO: this part is not yet correct i think
+for e in range(events.shape[0]):
+    # Get all waterlevels within a drawdown event
+    wL_e = wL[events['h1_t'][e]:events['h2_t'][e]]
+    print wL_e
+    
+    # Get delta t (dt) and delta waterlevel (dwL) (=single drawdown values)
+    dwL = []
+    dt = []
+    for i in range(len(wL_e)):
+        if i != 0:
+            dwL.append(wL_e.values[i-1] - wL_e.values[i])
+            dt.append(wL_e.index[i] - wL_e.index[i-1])
+    dwL_e = pd.DataFrame({'dwL' : dwL,
+                          'dt' : np.cumsum(dt)})
+    dwL_e['dt'] = dwL_e['dt'] / np.timedelta64(1, 'm')    # convert timedelta64 to float
+    print dwL_e
+    
+    # Fit straight line to the drawdown values on semilog scale
+    x = dwL_e['dt']
+    x_ln = np.log(x)
+    y = dwL_e['dwL']
+    a, b = np.polyfit(x_ln, y, 1)
+    
+    # Compute transmissivity (T) over one logarithmic cycle
+    y_max = a * 1 + b
+    y_min = a * 10 + b
+    dsw = y_max - y_min   # [m]
+    T = 2.303 * Q / (4 * np.pi * dsw)
+    print T
+    '''
+    # Plot drawdown vs time on semilog scale
+    plt.figure()
+    #plt.plot(x_ln, a * x_ln + b, '--')
+    x_plt = np.array(range(1,11,1)).reshape((10, 1))
+    plt.plot(x_plt, a * x_plt + b, 'b-')
+    plt.plot(x_ln, y, 'go', label='water level')
+    #plt.plot(1, y_max, 'bo')
+    #plt.plot(10, y_min, 'bo')
+    plt.title('Pisciarelli Tennis Club (KELLER DCX22)')
+    plt.xlabel('Time')
+    plt.ylabel('Drawdown [m]')
+    plt.legend(loc='best')
+    #plt.xscale('log')
+    plt.grid()
+    plt.tight_layout()
+    plt.savefig(wdir + 'pisc_wL_semilog_' + str(e) + '.png' , dpi=300)
+    plt.show()
+    plt.close()
+    '''
+####################
+### Plot results ###
+####################
 fig = plt.figure()
-ax1 = fig.add_subplot(1,1,1)
-wL_plt = wL.resample(L, how=stat).plot(ax=ax1, kind='line', color='red', label = 'wL',
-                                       title='Pisciarelli Tennis Club (KELLER DCX22 - ' + L + ' ' + stat + ')')
-#wL_plt = wL.plot(ax=ax1, kind='line', color='black',
-#                 label='water level', title='Pisciarelli Tennis Club (KELLER DCX22)') #logx=True)
-wL_plt.set_xlabel('Time')
-wL_plt.set_ylabel('wL# [cm]')
 
-# plot air pressure timeseries
+ax1 = fig.add_subplot(1,1,1)
 ax2 = ax1.twinx()
-aP_plt = aP.resample(L, how=stat).plot(ax=ax2, kind='line', color='blue', label = 'aP',)
-aP_plt.set_ylabel('aP [bar]')
+
+# Plot waterlevel timeseries VS air pressure timeseries
+if t_res is True:
+    # Waterlevel
+    wL_plt = wL.plot(ax=ax1, kind='line', color='blue', label = 'wL', 
+            title='Pisciarelli Tennis Club (KELLER DCX22 - ' + sr + ' ' + stat + ')')
+    plt_save = wdir + 'pisc_wL_aP_' + sr + stat + '.png'
+else:
+    # Waterlevel
+    wL_plt = wL.plot(ax=ax1, kind='line', color='blue', label = 'wL', 
+            title='Pisciarelli Tennis Club (KELLER DCX22)')   
+    plt_save = wdir + 'pisc_wL_aP.png'  
+wL_plt.set_xlabel('Time')
+wL_plt.set_ylabel('wL [m]')
+# Air pressure
+aP_plt = aP.plot(ax=ax2, kind='line', color='red', label = 'aP',)
+aP_plt.set_ylabel('aP [mH2O]')
 
 ax1.legend(loc='lower left')
 ax2.legend(loc='lower right')
-
+plt.grid()
 plt.tight_layout()
-plt.savefig(wdir + 'pisc_wL_aP_T_' + L + stat + '.png', dpi=300)
+plt.savefig(plt_save, dpi=300)
 plt.show()
-#######################################################
+plt.close()
 
+# Plot waterlevel timeseries with event markers
+if t_res is True:
+    plt.plot(wL.index, wL.values, 'b-', label='water level')
+    plt.title('Pisciarelli Tennis Club (KELLER DCX22 - ' + sr + ' ' + stat + ')')
+    plt_save = wdir + 'pisc_wL_events_' + sr + stat + '.png'
+else:
+    plt.plot(wL.index, wL.values, 'b-', label='water level')
+    plt.title('Pisciarelli Tennis Club (KELLER DCX22)')
+    plt_save = wdir + 'pisc_wL_events.png'  
+# Add event markers
+plt.plot(events['h1_t'], events['h1'], 'r+', mew=2, ms=8, label='h1')
+plt.plot(events['h2_t'], events['h2'], 'g+', mew=2, ms=8, label='h2')
+#plt.xticks(rotation=15)
+plt.xlabel('Time')
+plt.ylabel('wL [m]')
+plt.legend(loc='best')
+plt.grid()
+plt.tight_layout()
+plt.savefig(plt_save, dpi=300)
+plt.show()
+plt.close()
+
+'''
+plt.plot(dwL_e['dt'], dwL_e['dwL'], 'bo', label='water level')
+# TODO: plot the fitted straight line
+plt.plot(dwL_e['dt'], a * dwL_e['dt'] + b, 'r-')
+plt.title('Pisciarelli Tennis Club (KELLER DCX22)')
+plt.xlabel('Time [min]')
+plt.ylabel('Drawdown [m]')
+plt.legend(loc='best')
+plt.xscale('log')
+plt.grid()
+plt.tight_layout()
+plt.savefig(wdir + 'pisc_wL_semilogTEST.png' , dpi=300)
+plt.show()
+plt.close()
+'''
 # Get run time
 endtime = time.time()
 time_total = endtime-starttime
