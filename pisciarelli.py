ad11ab6d9a6c8b3ea4f927eb3750740512b17bae
MWieland <mwieland@gfz-potsdam.de>
2016-02-22 17:00:42 +0100

    added more options for user parameters

--- pisciarelli.py
+++ pisciarelli.py
@@ -6,10 +6,10 @@ Created on 08.02.2016
 Last modified on 22.02.2016
 Author: Marc Wieland
 Description: Single-well pump test to derive transmissivity and storage coefficient from waterlevel measurements.
-             0. Convert units of input data to [m]
+             0. Convert units of input data to [m] (optional)
              1. Resample and/or slice timeseries (optional)
              2. Correct waterlevel with airpressure data (optional)
-             3. Convert water level to [m below ground]
+             3. Convert water level to [m below ground] (optional)
              4. Auto-detect events in waterlevel timeseries.
              5. Separate waterlevels for each event between drawdown and fill
              6. Interpolate drawdown and fill parts separately (optional)
@@ -37,17 +37,29 @@ import matplotlib.pyplot as plt
 import timeseries.helper as helper
 
 # Parameters to set ########################################################################################################
-wdir = '/media/datadrive_/projects/medsuv_heiko/exp_pitc/marc/dcx22/' # Work directory
-dat_wl = 'PITC (DCX22).csv' # Water level data [mbar]
+site_name = 'Pisciarelli Tennis Club'
+instrument = 'EDL'
 ###
-c_ap = True # Correct water level with air pressure data
-dat_bar = 'AGN3baro.csv'    # Air pressure data [cmH2O]
+wdir = '/media/datadrive_/projects/medsuv_heiko/exp_pitc/marc/edl/' # Work directory
+dat_wl = 'PITC (EDL).csv' # File that holds the water level data 
+col_wl_t = 'longDATE'  # Column that holds the measurement timestamps
+col_wl = 'wL_m' # Column that holds the water level measures in [m] or [mbar]
+mbar2m = False  # Convert water level measures from [mbar] to [m]
+###
+c_ap = False    # Correct water level with air pressure data
+dat_ap = 'AGN3baro.csv'    # File that holds the air pressure data
+col_ap_t = 'longDATE'  # Column that holds the measurement timestamps
+col_ap = 'aP'   # Column that holds the air pressure measures in [mH2O] or [cmH2O]
+cm2m = False    # Convert air pressure measures from [cmH2O] to [mH2O]
+###
+c_g = False     # Convert water level to meters below ground
+g = 33.7        # Ground level [m]
 ###
 t_slice = True    # Temporal slicing
-slice_t1 = '2014-06-30 00:01'   # Slicing start time
-slice_t2 = '2014-10-30 23:59'   # Slicing end time
+slice_t1 = '2014-05-19 13:00'   # Slicing start time
+slice_t2 = '2014-05-20 13:00'   # Slicing end time
 ###
-t_res = True    # Temporal resampling 
+t_res = False   # Temporal resampling 
 sr = "10min"    # Temporal resampling rate (e.g., 5min, H, D, M)
 stat = 'mean'   # Temporal resampling statistics 
 ###
@@ -62,7 +74,6 @@ peak_lookahead = 1  # Event detection: distance to look ahead from a peak candid
 ###
 Q = 0.006944    # Pumping rate [cubic meters per second]
 r = 0.5         # Radius [m]
-g = 33.7        # Ground level [m] (used to compute meters below ground)
 ############################################################################################################################
 
 starttime=time.time()
@@ -74,8 +85,10 @@ print 'Starttime : ' + str(time.strftime("%H:%M:%S"))
 # Create waterlevel timeseries
 df1 = pd.read_table(wdir + dat_wl, header=0, delimiter=';', parse_dates=True) 
 dates1 = pd.DatetimeIndex(df1['longDATE'].values)
-df1['wL#'] = df1['wL#'] * 0.0101972   # convert waterlevel (wL) from [mbar] to [m]
-wL = pd.Series(df1['wL#'].values, index=dates1)
+if mbar2m is True:
+    # Convert waterlevel (wL) from [mbar] to [m]
+    df1[col_wl] = df1[col_wl] * 0.0101972
+wL = pd.Series(df1[col_wl].values, index=dates1)
 if t_slice is True:
     # Slice waterlevel timeseries
     wL = wL[slice_t1:slice_t2]
@@ -85,10 +98,12 @@ if t_res is True:
 
 if c_ap is True:
     # Create airpressure timeseries
-    df2 = pd.read_table(wdir + dat_bar, header=0, delimiter=';', parse_dates=True) 
+    df2 = pd.read_table(wdir + dat_ap, header=0, delimiter=';', parse_dates=True) 
     dates2 = pd.DatetimeIndex(df2['longDATE'].values)
-    df2['aP'] = df2['aP'] / 100     # convert airpressure (aP) from [cmH2O] to [mH2O]
-    aP = pd.Series(df2['aP'].values, index=dates2)
+    if cm2m is True:
+        # Convert airpressure (aP) from [cmH2O] to [mH2O]
+        df2[col_ap] = df2[col_ap] / 100
+    aP = pd.Series(df2[col_ap].values, index=dates2)
     if t_slice is True:
         # Slice airpressure timeseries
         aP = aP[slice_t1:slice_t2]
@@ -98,8 +113,9 @@ if c_ap is True:
     # Correct waterlevel with airpressure data
     wL = wL - aP
 
-# Convert waterlevel to meters below ground
-wL = wL - g
+if c_g is True:
+    # Convert waterlevel to meters below ground
+    wL = wL - g
 
 #######################
 ### Event detection ###
@@ -216,25 +232,25 @@ for e in range(len(eid)):
     plt.plot(wL_d[wL_d['event'] == eid[e]].index, wL_d[wL_d['event'] == eid[e]]['wL'].values, 'bo')
     plt.plot(wL_f[wL_f['event'] == eid[e]].index, wL_f[wL_f['event'] == eid[e]]['wL'].values, 'g-', label='water level')
     plt.plot(wL_f[wL_f['event'] == eid[e]].index, wL_f[wL_f['event'] == eid[e]]['wL'].values, 'go')
-    plt.title('Pisciarelli Tennis Club (KELLER DCX22 - ' + sr + ' ' + stat + ') - Event ' + str(eid[e]))
+    plt.title(site_name + '(' + instrument + ') - Event ' + str(eid[e]))
     plt.xlabel('Time')
     plt.ylabel('wL [m]')
     plt.grid()
     plt.tight_layout()
-    plt.savefig(wdir + 'pisc_wL_event_' + str(eid[e]) + '.png', dpi=300)
+    plt.savefig(wdir + 'wL_event_' + str(eid[e]) + '.png', dpi=300)
     #plt.show()
     plt.close()
     
     # Plot event straight line fit
     plt.plot(x, a * x + b, '--')
     plt.plot(x, y, 'ro')
-    plt.title('Pisciarelli Tennis Club (KELLER DCX22 - ' + sr + ' ' + stat + ') - Event ' + str(eid[e]))
+    plt.title(site_name + '(' + instrument + ') - Event ' + str(eid[e]))
     plt.annotate('T = ' + str(T) + ' m^2/s', xy=(1000, 1000), xytext=(1000, 1000))
     plt.xlabel('X')
     plt.ylabel('Y')
     plt.grid()
     plt.tight_layout()
-    plt.savefig(wdir + 'pisc_linefit_event_' + str(eid[e]) + '.png', dpi=300)
+    plt.savefig(wdir + 'linefit_event_' + str(eid[e]) + '.png', dpi=300)
     #plt.show()
     plt.close()
 
@@ -247,11 +263,11 @@ ax2 = ax1.twinx()
 
 # Plot waterlevel timeseries with event markers and airpressure data
 if t_res is True:
-    title='Pisciarelli Tennis Club (KELLER DCX22 - ' + sr + ' ' + stat + ')'
-    plt_save = wdir + 'pisc_wL_' + sr + stat + '.png'    
+    title = site_name + '(' + instrument + ' - ' + sr + ' ' + stat + ')'
+    plt_save = wdir + 'wL_' + sr + stat + '.png'    
 else:
-    title='Pisciarelli Tennis Club (KELLER DCX22)'
-    plt_save = wdir + 'pisc_wL.png'
+    title = site_name + '(' + instrument + ')'
+    plt_save = wdir + 'wL.png'
 ax1.plot(wL.index, wL.values, 'b-', label='wL')
 # Add event markers
 ax1.plot(events['h1_t'], events['h1'], 'r+', mew=2, ms=8, label='h1')
@@ -276,30 +292,42 @@ plt.close()
 events = events[events['T'] != 0.0]
 
 # Plot Transmissivity (T) over all events
+if t_res is True:
+    title = site_name + '(' + instrument + ' - ' + sr + ' ' + stat + ')'
+    plt_save = wdir + 'transmissivity_' + sr + stat + '.png'    
+else:
+    title = site_name + '(' + instrument + ')'
+    plt_save = wdir + 'transmissivity.png'
 plt.plot(events['h2_t'], events['T'], 'r-')
 #plt.plot(events['h2_t'], events['T'], 'ro')
-plt.title('Pisciarelli Tennis Club (KELLER DCX22 - ' + sr + ' ' + stat + ')')
+plt.title(title)
 plt.xlabel('Time')
 plt.ylabel('Transmissivity [m^2/s]')
 #plt.ylim(0, 0.0025)
 plt.xticks(rotation=15)
 plt.grid()
 plt.tight_layout()
-plt.savefig(wdir + 'pisc_transmissivity.png', dpi=300)
+plt.savefig(plt_save, dpi=300)
 #plt.show()
 plt.close()
 
 # Plot Storage coefficient (S) over all events
+if t_res is True:
+    title = site_name + '(' + instrument + ' - ' + sr + ' ' + stat + ')'
+    plt_save = wdir + 'storagecoefficient_' + sr + stat + '.png'    
+else:
+    title = site_name + '(' + instrument + ')'
+    plt_save = wdir + 'storagecoefficient.png'
 plt.plot(events['h2_t'], events['S'], 'r-')
 #plt.plot(events['h2_t'], events['S'], 'ro')
-plt.title('Pisciarelli Tennis Club (KELLER DCX22 - ' + sr + ' ' + stat + ')')
+plt.title(title)
 plt.xlabel('Time')
 plt.ylabel('Storage coefficient [-]')
 #plt.ylim(-25, 25)
 plt.xticks(rotation=15)
 plt.grid()
 plt.tight_layout()
-plt.savefig(wdir + 'pisc_storagecoefficient.png', dpi=300)
+plt.savefig(plt_save, dpi=300)
 #plt.show()
 plt.close()
 
@@ -309,15 +337,15 @@ plt.close()
 print events
 
 # Write events dataframe to csv
-events.to_csv(wdir + 'pisc_events.csv', sep=';')
+events.to_csv(wdir + 'events.csv', sep=';')
 
 # Write water levels for drawdown and fill events to csv
 if wL_d is not None:
     wL_d.columns = ['wL', 'event']
-    wL_d.to_csv(wdir + 'pisc_events_wL_drawdown.csv', sep=';')
+    wL_d.to_csv(wdir + 'events_wL_drawdown.csv', sep=';')
 if wL_f is not None:
     wL_f.columns = ['wL', 'event']
-    wL_f.to_csv(wdir + 'pisc_events_wL_fill.csv', sep=';')
+    wL_f.to_csv(wdir + 'events_wL_fill.csv', sep=';')
 
 # Get run time
 endtime = time.time()
