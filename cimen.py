73bbd6b4e864d5a021a23d1abe24908132767f3f
MWieland <marcwieland@gmx.de>
2016-04-04 10:17:03 +0100

    fixed error in equation for S

--- cimen.py
+++ cimen.py
@@ -1,7 +1,7 @@
 '''
 ---------------------------
-    pisciarelli.py
----------------------------
+    Cimen.py
+--------------------------
 Created on 08.02.2016
 Last modified on 23.02.2016
 Author: Marc Wieland
@@ -16,7 +16,7 @@ Description: Single-well pump test to derive transmissivity and storage coeffici
              7. Compute Transmissivity (T) in [m^2/s]:
                  T = Q / (4 * np.pi * a)
                 and Storage coefficient (S) in [m] according to Zheng et al.:
-                 S = -(4 * T * b) / (a * (r * r))
+                 S = -4 * T * b / a * (r * r)
                  Q = pumping rate
                  a = slope of straight line fit
                  b = intercept of straight line fit
@@ -37,34 +37,34 @@ import matplotlib.pyplot as plt
 import timeseries.helper as helper
 
 # Parameters to set ########################################################################################################
-site_name = 'Pisciarelli Tennis Club'
-instrument = 'KELLER DCX22'
+site_name = 'Cimen'
+instrument = 'Synthetic data'
 ###
-wdir = '/media/marc/GFZDRIVE/datadrive/projects/medsuv_heiko/exp_pitc/marc/dcx22_test/' # Work directory
-dat_wl = 'PITC (DCX22).csv' # File that holds the water level data 
+wdir = '/media/marc/GFZDRIVE/datadrive/projects/medsuv_heiko/synthetic_test/' # Work directory
+dat_wl = 'Cimen.csv' # File that holds the water level data 
 col_wl_t = 'longDATE'  # Column that holds the measurement timestamps
-col_wl = 'wL#' # Column that holds the water level measures in [m] or [mbar]
-mbar2m = True  # Convert water level measures from [mbar] to [m]
+col_wl = 'wL_m' # Column that holds the water level measures in [m] or [mbar]
+mbar2m = False  # Convert water level measures from [mbar] to [m]
 ###
-c_ap = True    # Correct water level with air pressure data
+c_ap = False    # Correct water level with air pressure data
 dat_ap = 'AGN3baro.csv'    # File that holds the air pressure data
 col_ap_t = 'longDATE'  # Column that holds the measurement timestamps
 col_ap = 'aP'   # Column that holds the air pressure measures in [mH2O] or [cmH2O]
 cm2m = True    # Convert air pressure measures from [cmH2O] to [mH2O]
 ###
-c_g = True     # Convert water level to meters below ground
+c_g = False     # Convert water level to meters below ground
 g = 33.7        # Ground level [m]
 ###
-t_slice = True    # Temporal slicing
-slice_t1 = '2014-06-30 00:01'   # Slicing start time
-slice_t2 = '2014-06-30 23:59'   # Slicing end time
+t_slice = False    # Temporal slicing
+slice_t1 = '2014-05-19 13:00'   # Slicing start time
+slice_t2 = '2014-05-20 00:00'   # Slicing end time
 ###
-t_res = True   # Temporal resampling 
+t_res = False   # Temporal resampling 
 sr = "10min"    # Temporal resampling rate (e.g., 5min, H, D, M)
 stat = 'mean'   # Temporal resampling statistics 
 ###
 t_int = False   # Temporal interpolation 
-ir = "1min"     # Temporal interpolation rate (e.g., 5min, H, D, M)
+ir = "10min"     # Temporal interpolation rate (e.g., 5min, H, D, M)
 if_d = 'cubic'  # Temporal interpolation function for drawdown events 
 if_f = 'cubic'  # Temporal interpolation function for fill events                
                 # {'linear', 'time', 'index', 'values', 'nearest', 'zero', 'slinear', , 'quadratic',
@@ -72,8 +72,12 @@ if_f = 'cubic'  # Temporal interpolation function for fill events
 ###
 peak_lookahead = 1  # Event detection: distance to look ahead from a peak candidate to determine the actual peak
 ###
-Q = 0.006944    # Pumping rate [cubic meters per second]
-r = 0.5         # Radius [m]
+Q = 0.05        # Pumping rate [cubic meters per second]
+r = 50          # Radius [m]
+
+# Suppress SettingWithCopyWarning that appears in this script as a false positive when doing
+# something like events['T'][events.index == eid[e]] = T
+pd.options.mode.chained_assignment = None
 ############################################################################################################################
 
 starttime=time.time()
@@ -116,6 +120,7 @@ if c_ap is True:
 if c_g is True:
     # Convert waterlevel to meters below ground
     wL = wL - g
+#wL = wL * -1
 
 #######################
 ### Event detection ###
@@ -185,17 +190,15 @@ if wL_f is not None:
 events['T'] = 0
 events['S'] = 0
 
-# Suppress SettingWithCopyWarning that appears in this script as a false positive when doing
-# events['T'][events.index == eid[e]] = T
-pd.options.mode.chained_assignment = None
-
 # Get fill event ids
 eid = wL_f['event'].unique()
 
 for e in range(len(eid)):
     # Get tp for fill event (converted to float and unit in [s])
+    # tp is the temporal offset since the last fill event (this equals the drawdown time)
     tp = events[events.index == eid[e]]['dt'] / np.timedelta64(1, 's')
     tp = tp.values[0]
+    print 'tp: ' + str(tp)
     
     # Get waterlevels within fill event
     wL_e = wL_f[wL_f['event'] == eid[e]]
@@ -208,27 +211,40 @@ for e in range(len(eid)):
         if i != 0:
             dwL.append(wL_e['wL'].values[i] - wL_e['wL'].values[i-1])
             dt.append(wL_e.index[i] - wL_e.index[i-1])
+            print dt[-1]
     
     # Compute cummulative dwL and dt (note: this would be "s'" and "t'" in Zheng et al.)
     dwL_e = pd.DataFrame({'dwL' : np.cumsum(dwL),
                           'dt' : np.cumsum(dt)})
+                          
+    
     # Convert timedelta64 to float and unit in [s]
     dwL_e['dt'] = dwL_e['dt'] / np.timedelta64(1, 's')
     dwL_e['tp'] = tp
-    #print dwL_e
+    print dwL_e
+
+    #print wL_e['wL']
+    #print dwL_e['dt']       
+    #print dwL_e['dwL']   
     
     # Fit straight line
     x = dwL_e['dt'] * np.log(dwL_e['dt'] + dwL_e['tp'] / dwL_e['dt'])
     y = dwL_e['dt'] * dwL_e['dwL']
+        
+    #print x
+    #print y
     
     a, b = np.polyfit(x, y, 1)
     
+    #print a
+    #print b
+        
     # Compute transmissivity (T) in [m^2/s]
     T = Q / (4 * np.pi * a)
     events['T'][events.index == eid[e]] = T
     
     # Compute storage coefficient (S) in [m]
-    S = -(4 * T * b) / (a * (r * r))
+    S = (-4 * T * b) / (a * (r * r))
     events['S'][events.index == eid[e]] = S
     
     # Plot event water level
@@ -263,9 +279,9 @@ for e in range(len(eid)):
 ####################
 fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
-ax2 = ax1.twinx()
+#ax2 = ax1.twinx()
 
-# Plot waterlevel timeseries with event markers and airpressure data
+# Plot waterlevel timeseries with event markers
 if t_res is True:
     title = site_name + ' (' + instrument + ' - ' + sr + ' ' + stat + ')'
     plt_save = wdir + 'wL_' + sr + stat + '.png'    
@@ -278,11 +294,11 @@ ax1.plot(events['h1_t'], events['h1'], 'r+', mew=2, ms=8, label='h1')
 ax1.plot(events['h2_t'], events['h2'], 'g+', mew=2, ms=8, label='h2')
 ax1.set_ylabel('wL [m]')
 ax1.legend(loc='lower left')
-if c_ap is True:
+#if c_ap is True:
     # Add air pressure
-    ax2.plot(aP.index, aP.values, 'r-', label='aP')
-    ax2.set_ylabel('aP [mH2O]')
-    ax2.legend(loc='lower right')
+    #ax2.plot(aP.index, aP.values, 'r-', label='aP')
+    #ax2.set_ylabel('aP [mH2O]')
+    #ax2.legend(loc='lower right')
 #plt.xticks(rotation=15)
 plt.title(title)
 plt.xlabel('Time')
@@ -355,59 +371,3 @@ if wL_f is not None:
 endtime = time.time()
 time_total = endtime-starttime
 print str(time_total) + ' sec'
-
-'''
-##########################################################
-### Compute transmissivity (T) (Cooper and Jacob 1946) ###
-##########################################################
-for e in range(events.shape[0]):
-    # TODO: this part is not yet correct i think
-    # Get all waterlevels within a drawdown event
-    wL_e = wL[events['h1_t'][e]:events['h2_t'][e]]
-    print wL_e
-    
-    # Get delta t (dt) and delta waterlevel (dwL) (=single drawdown values)
-    dwL = []
-    dt = []
-    for i in range(len(wL_e)):
-        if i != 0:
-            dwL.append(wL_e.values[i-1] - wL_e.values[i])
-            dt.append(wL_e.index[i] - wL_e.index[i-1])
-    dwL_e = pd.DataFrame({'dwL' : dwL,
-                          'dt' : np.cumsum(dt)})
-    dwL_e['dt'] = dwL_e['dt'] / np.timedelta64(1, 'm')    # convert timedelta64 to float
-    print dwL_e
-    
-    # Fit straight line to the drawdown values on semilog scale
-    x = dwL_e['dt']
-    x_ln = np.log(x)
-    y = dwL_e['dwL']
-    a, b = np.polyfit(x_ln, y, 1)
-    
-    # Compute transmissivity (T) over one logarithmic cycle
-    y_max = a * 1 + b
-    y_min = a * 10 + b
-    dsw = y_max - y_min   # [m]
-    T = 2.303 * Q / (4 * np.pi * dsw)
-    print T
-'''
-'''
-    # Plot drawdown vs time on semilog scale
-    plt.figure()
-    #plt.plot(x_ln, a * x_ln + b, '--')
-    x_plt = np.array(range(1,11,1)).reshape((10, 1))
-    plt.plot(x_plt, a * x_plt + b, 'b-')
-    plt.plot(x_ln, y, 'go', label='water level')
-    #plt.plot(1, y_max, 'bo')
-    #plt.plot(10, y_min, 'bo')
-    plt.title('Pisciarelli Tennis Club (KELLER DCX22)')
-    plt.xlabel('Time')
-    plt.ylabel('Drawdown [m]')
-    plt.legend(loc='best')
-    #plt.xscale('log')
-    plt.grid()
-    plt.tight_layout()
-    plt.savefig(wdir + 'pisc_wL_semilog_' + str(e) + '.png' , dpi=300)
-    plt.show()
-    plt.close()
-'''
